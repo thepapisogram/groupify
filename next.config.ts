1a5441b7cf9f9a6e608f6533557ab2bbe3749116
Anthony Saah <thepapisogram@gmail.com>
2024-10-27 22:07:08 +0000

    config file update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  images: {
+    localPatterns: [
+      {
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
-
-export default nextConfig;
