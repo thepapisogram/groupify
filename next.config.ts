37fc814b172c0cb027bab04a664b4258e273a0bf
Anthony Saah <thepapisogram@gmail.com>
2024-10-27 22:12:39 +0000

    config file update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 module.exports = {
   images: {
-    localPatterns: [
+    remotePatterns: [
       {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000", // or your development server port
         pathname: "/**",
-        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "https://groupify-tool.netlify.app", // replace with your live domain
+        pathname: "/**",
       },
     ],
   },
