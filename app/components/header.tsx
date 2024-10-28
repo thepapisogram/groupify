24855b8b054c1f0af8ef728de9113e989abde8dc
Anthony Saah <thepapisogram@gmail.com>
2024-10-28 02:23:14 +0000

    minor changes

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,12 +7,16 @@ const Header = ({ title, subtitle}:{title:string, subtitle:string}) => {
         <div className="flex items-center justify-center gap-x-3">
           <Image
             className="block"
-            src="./logo.png"
+            src={"https://groupify-tool.netlify.app/logo.png"}
             width={50}
             height={50}
             alt="Groupify Logo"
           />
-          <h1 className={`${rubik_gemstones.className} text-sky-600 dark:text-green-600 text-3xl md:text-5xl my-auto`}>{title}</h1>
+          <h1
+            className={`${rubik_gemstones.className} text-sky-600 dark:text-green-600 text-3xl md:text-5xl my-auto`}
+          >
+            {title}
+          </h1>
         </div>
         <h2 className="text-stone-900 dark:text-zinc-300 font-medium mt-1 text-md md:text-lg">
           {subtitle}
