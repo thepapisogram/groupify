0be20cbff1df5777b228b6bdef9550a65f6e230c
Anthony Saah <thepapisogram@gmail.com>
2024-11-01 19:11:54 +0000

    version 1.5.1 bug fix

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    domains: ["ajeuwbhvhr.cloudimg.io", "colony-recorder.s3.amazonaws.com"],
   },
 };
 
